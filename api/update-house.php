a2c4e4c5154a84dbad0190348eeae8c4a367ef8b
user-z55554dfgfh <novarix.studio@gmail.com>
2025-05-20 02:34:06 +0200

    Create update-house.php

---
+++ api/update-house.php
@@ -0,0 +1,24 @@
+<?php
+// Optionaler Schutz mit secret Token:
+if ($_GET['token'] !== 'dein_geheimer_token') {
+    http_response_code(403);
+    echo json_encode(['error' => 'Unauthorized']);
+    exit;
+}
+
+$hausnummer = $_GET['hausnummer'];
+$ort = $_GET['ort'];
+$filename = '../data/haeuser.json';
+$haeuser = json_decode(file_get_contents($filename), true);
+
+foreach ($haeuser as &$haus) {
+    if ($haus['hausnummer'] == $hausnummer && $haus['ort'] === $ort) {
+        $haus['verkauft'] = true;
+        file_put_contents($filename, json_encode($haeuser, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+        echo json_encode(['success' => true]);
+        exit;
+    }
+}
+
+http_response_code(404);
+echo json_encode(['error' => 'Haus nicht gefunden']);
